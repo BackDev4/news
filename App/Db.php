74b78c58d4b845a95855bb0954369f0a46d2b238
IliaOsipov4 <98261622+IliaOsipov4@users.noreply.github.com>
2022-06-28 17:26:48 +0300

    Add files via upload

--- App/Db.php
+++ App/Db.php
@@ -5,34 +5,33 @@ namespace App;
 class Db
 {
 
-    protected  $connect;
+    protected $connect;
 
     public function __construct()
     {
         $config = (include __DIR__ . '/../config.php') ['db'];
         $this->connect =
             new \PDO('mysql:host=' . $config['host'] . ';dbname=' . $config['dbname'],
-                 $config['user'], $config['password']
+                $config['user'], $config['password']
             );
     }
 
-    public function query($sql, $class)
+    public function query($sql, $data = [], $class)
     {
         $sth = $this->connect->prepare($sql);
-        $sth->execute(array());
-        return $sth->fetchAll();
-//        $ret = [];
-//        foreach ($data as $row) {
-//            $item = new $class;
-//            foreach ($row as $key => $val) {
-//                $item->$key = $val;
-//                if (is_numeric($key)) {
-//                    continue;
-//                }
-//                $item->$key = $val;
-//            }
-//            $ret[] = $item;
-//        }
-//        return $ret;
+        $sth->execute($data);
+        return $sth->fetchAll(\PDO::FETCH_CLASS, $class);
     }
+
+    public function execute($sql, $data = [])
+    {
+        $sth = $this->connect->prepare($sql);
+        return $sth->execute($data);
+    }
+
+    public function getLastId()
+    {
+        return $this->connect->lastInsertId();
+    }
+
 }
--- App/Model.php
+++ App/Model.php
@@ -6,7 +6,6 @@ abstract class Model
 {
 
 
-
     public const TABLE = '';
 
     public $id;
@@ -14,9 +13,37 @@ abstract class Model
     public static function findAll()
     {
         $db = new Db();
-        $sql = sprintf("SELECT * FROM %s", static::TABLE);
+        $sql = sprintf('SELECT * FROM %s', static::TABLE);
         return $db->query($sql, [], static::class);
 
+    }
+
+    public function insert()
+    {
+        $fields = get_object_vars($this);
+
+        $cols = [];
+        $data = [];
+
+        foreach ($fields as $name => $value) {
+
+            if ('id' == $name) {
+                continue;
+            }
+            $cols[] = $name;
+            $data [':' . $name] = $value;
+
+        }
+
+        $sql = 'INSERT INTO ' . static::TABLE . '
+         (' . implode(',', $cols) . ')
+        VALUES
+        (' . implode(',', array_keys($data)) . ')
+        ';
+
+        $db = new Db();
+        $db->execute($sql, $data);
+        $this->id = $db->getLastId();
     }
 
 }
