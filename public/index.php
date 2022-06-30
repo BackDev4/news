864661b31ed4565c20e3b5ba5388ab1ce0c7ab32
IliaOsipov4 <98261622+IliaOsipov4@users.noreply.github.com>
2022-06-30 13:16:27 +0300

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -2,9 +2,17 @@
 
 require '../App/autoload.php';
 
-$controller = $_GET['controller'] ?? 'Index';
-$class =  '\App\Controllers\\' . $controller;
+$uri = $_SERVER['REQUEST_URI'];
+$parts = explode('/', $uri);
+$controller = $parts[1] ? ucfirst($parts[1]) : 'Index';
 
-$controller = new $class;
-$controller();
-
+try {
+    $controller = $_GET['controller'] ?? 'Index';
+    $class = '\App\Controllers\\' . $controller;
+    $controller = new $class;
+    $controller();
+} catch (\App\DbExeption $error) {
+    echo 'Ошибка БД:' . $error->getMessage();
+}catch (Exception $error){
+    echo 'Неизвестная ошибка';
+}
